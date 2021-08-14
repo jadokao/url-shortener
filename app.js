a49b0aa20f0ca1b3af744b34a878abcfd0cb23fd
jadokao <jackykao528@outlook.com>
2021-08-14 17:27:01 +0800

    feat: basic layouts

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
 const port = 3000
 
+app.use(express.static('public'))
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index', { style: 'style.css' })
 })
 
 app.listen(port, () => {
