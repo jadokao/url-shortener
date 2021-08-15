6cf85dc920704f8b3bf815d6fc1526ca38c33d2e
jadokao <jackykao528@outlook.com>
2021-08-15 19:57:00 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const bodyParser = require('body-parser')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 // import mongoDB
 require('./config/mongoose')
