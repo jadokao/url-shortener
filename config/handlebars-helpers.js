cd738dc0fbf2855d773041ff12d57c629cd180fb
jadokao <jackykao528@outlook.com>
2021-08-26 20:32:40 +0800

    update: fix copy function

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,32 +1,16 @@
-// const { clipboard } = require('electron')
+// const jsdom = require('jsdom')
+// const { JSDOM } = jsdom
 
 // module.exports = {
-//   copy: function (text) {
-//     if (text) {
-//       clipboard.writeText(text)
-//     }
+//   copy: function () {
+//     const dom = new JSDOM(
+//       `<button type="button" id="copy" class="btn btn-primary" onclick="{{#copy shortURL}}{{/copy}}">Handlebars-helper: Copy</button>`
+//     )
+//     const document = dom.window.document
+//     /* Get the text */
+//     var copyText = document.getElementById('copy').innerHTML
+//     /* Copy the text inside the text field */
+//     navigator.clipboard.writeText(copyText)
+//     console.log('copy')
 //   },
 // }
-
-// Copy to clipboard in node.js
-const child_process = require('child_process')
-// var ncp = require('copy-paste').global()
-
-module.exports = {
-  copy: function (url) {
-    // This uses an external application for clipboard access, so fill it in here
-    // Some options: pbcopy (macOS), xclip (Linux or anywhere with Xlib)
-    // const COPY_APP = 'xclip'
-    // const proc = child_process.spawn(COPY_APP)
-    // proc.stdin.write(url, { encoding: 'utf8' })
-    // proc.stdin.end()
-    // console.log('copy')
-
-    // ncp.copy(url)
-    console.log(url)
-
-    // const proc = require('child_process').spawn('pbcopy')
-    // proc.stdin.write(url)
-    // proc.stdin.end()
-  },
-}
